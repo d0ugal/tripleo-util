a8da898dbc3666188f8a6079f933a8d06bd40f9f
Dougal Matthews <dougal@redhat.com>
2016-06-14 11:45:45 +0100

    Chmod +x

--- undercloud/mistral-purge.sh
+++ undercloud/mistral-purge.sh
@@ -4,5 +4,7 @@ set -o pipefail
 
 exec_ids=$(mistral execution-list | awk -F "| " '{print $2; }')
 for exec_id in $exec_ids; do
-  echo $exec_id;
+  if [ ! $exec_id = "ID" ]; then
+    mistral execution-delete $exec_id
+  fi
 done
