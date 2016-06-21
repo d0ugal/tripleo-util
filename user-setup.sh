337f805873784728989e38df68684b4068e2442c
Dougal Matthews <dougal@dougalmatthews.com>
2016-06-21 10:28:27 +0100

    Update and rename host/0-user-setup.sh to user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 sudo yum upgrade -y
-sudo yum install -y tmux vim wget
+sudo yum install -y tmux vim wget git
 sudo useradd stack
 echo "stack ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/stack
 sudo chmod 0440 /etc/sudoers.d/stack
