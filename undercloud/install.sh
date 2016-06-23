467d66dfc01ab9cfd7dc9948ddbf818f4b7f4e80
Dougal Matthews <dougal@redhat.com>
2016-06-23 13:47:31 +0100

    Add the workaround package url

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -10,6 +10,10 @@ sudo yum install -y libffi-devel libssl-devel openssl-devel;
 
 rm -rf ~/tripleo-ci
 git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
+
+export DELOREAN_REPO_URL="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/"
+export DELOREAN_STABLE_REPO_URL="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-$STABLE_RELEASE/"
+
 ~/tripleo-ci/scripts/tripleo.sh --repo-setup
 ~/tripleo-ci/scripts/tripleo.sh --undercloud
 ~/tripleo-ci/scripts/tripleo.sh --overcloud-images
