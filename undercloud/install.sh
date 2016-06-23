5a4a3775645e5c6945aedbe03bcff7d1047d22dd
Dougal Matthews <dougal@redhat.com>
2016-06-23 13:34:25 +0100

    Use tripleo.sh

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -5,28 +5,10 @@ if [[ $USER != "stack" ]]; then
 	exit 1;
 fi
 
-sudo yum -y install epel-release
-sudo curl -o /etc/yum.repos.d/delorean.repo http://trunk.rdoproject.org/centos7/current-tripleo/delorean.repo
-sudo curl -o /etc/yum.repos.d/delorean-current.repo http://trunk.rdoproject.org/centos7/current/delorean.repo
-sudo sed -i 's/\[delorean\]/\[delorean-current\]/' /etc/yum.repos.d/delorean-current.repo
-sudo /bin/bash -c "cat <<EOF>>/etc/yum.repos.d/delorean-current.repo
-
-includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-cloud-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,tripleo-common,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements,openstack-puppet-modules
-EOF"
-sudo curl -o /etc/yum.repos.d/delorean-deps.repo http://trunk.rdoproject.org/centos7/delorean-deps.repo
-sudo yum -y install yum-plugin-priorities vim
-sudo yum install -y python-tripleoclient
-export DIB_INSTALLTYPE_puppet_modules=source
-openstack undercloud install
-
-source ~/stackrc
-export NODE_DIST=centos7
-export USE_DELOREAN_TRUNK=1
-export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7/current-tripleo/"
-export DELOREAN_REPO_FILE="delorean.repo"
-export DIB_INSTALLTYPE_puppet_modules=source
-openstack overcloud image build --all;
-openstack overcloud image upload;
+git clone https://github.com/openstack-infra/tripleo-ci.git
+./tripleo-ci/scripts/tripleo.sh --repo-setup
+./tripleo-ci/scripts/tripleo.sh --undercloud
+./tripleo-ci/scripts/tripleo.sh --overcloud-images
 
 wget https://bootstrap.pypa.io/get-pip.py;
 sudo python get-pip.py;
