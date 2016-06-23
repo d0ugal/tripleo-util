eb4dd18084601aa67741aaada7a23d3523255f60
Dougal Matthews <dougal@redhat.com>
2016-06-23 13:46:48 +0100

    Tidy up the install script

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -5,15 +5,18 @@ if [[ $USER != "stack" ]]; then
 	exit 1;
 fi
 
-git clone https://github.com/openstack-infra/tripleo-ci.git
-./tripleo-ci/scripts/tripleo.sh --repo-setup
-./tripleo-ci/scripts/tripleo.sh --undercloud
-./tripleo-ci/scripts/tripleo.sh --overcloud-images
+sudo yum upgrade -y;
+sudo yum install -y libffi-devel libssl-devel openssl-devel;
+
+rm -rf ~/tripleo-ci
+git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
+~/tripleo-ci/scripts/tripleo.sh --repo-setup
+~/tripleo-ci/scripts/tripleo.sh --undercloud
+~/tripleo-ci/scripts/tripleo.sh --overcloud-images
 
 wget https://bootstrap.pypa.io/get-pip.py;
 sudo python get-pip.py;
 sudo pip install virtualenv git-review;
+
 git clone https://github.com/openstack/python-tripleoclient.git;
 git clone https://github.com/openstack/tripleo-common.git;
-
-sudo yum install -y libffi-devel libssl-devel openssl-devel;
