9b8c68a1ed22bae66713e4c4e8770c3be1553e97
Dougal Matthews <dougal@redhat.com>
2016-06-27 10:55:27 +0100

    Comments

--- host/install.sh
+++ host/install.sh
@@ -5,14 +5,21 @@ if [[ $USER != "stack" ]]; then
 	exit 1;
 fi
 
+# Enable epel
 sudo yum -y install epel-release
+
+# Enable last known good RDO Trunk Delorean repository for core openstack packages
 sudo curl -o /etc/yum.repos.d/delorean.repo http://trunk.rdoproject.org/centos7/current-tripleo/delorean.repo
+
+# Enable latest RDO Trunk Delorean repository only for the TripleO packages
 sudo curl -o /etc/yum.repos.d/delorean-current.repo http://trunk.rdoproject.org/centos7/current/delorean.repo
 sudo sed -i 's/\[delorean\]/\[delorean-current\]/' /etc/yum.repos.d/delorean-current.repo
 sudo /bin/bash -c "cat <<EOF>>/etc/yum.repos.d/delorean-current.repo
 
 includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-cloud-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,tripleo-common,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements,openstack-puppet-modules
 EOF"
+
+# Enable the Delorean Deps repository
 sudo curl -o /etc/yum.repos.d/delorean-deps.repo http://trunk.rdoproject.org/centos7/delorean-deps.repo
 
 sudo yum install -y instack-undercloud
