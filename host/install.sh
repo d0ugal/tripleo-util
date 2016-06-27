05e651cb8bd14b52bda79e77e4e7f14fdc5aa881
Dougal Matthews <dougal@redhat.com>
2016-06-27 14:48:27 +0100

    Use tripleo.sh for the host

--- host/install.sh
+++ host/install.sh
@@ -5,21 +5,12 @@ if [[ $USER != "stack" ]]; then
 	exit 1;
 fi
 
-# Enable epel
-sudo yum -y install epel-release
+rm -rf ~/tripleo-ci
+git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
 
-# Enable last known good RDO Trunk Delorean repository for core openstack packages
-sudo curl -o /etc/yum.repos.d/delorean.repo http://trunk.rdoproject.org/centos7/current-tripleo/delorean.repo
+export DELOREAN_REPO_URL="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/"
+export DELOREAN_STABLE_REPO_URL="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-$STABLE_RELEASE/"
 
-# Enable latest RDO Trunk Delorean repository only for the TripleO packages
-sudo curl -o /etc/yum.repos.d/delorean-current.repo http://trunk.rdoproject.org/centos7/current/delorean.repo
-sudo sed -i 's/\[delorean\]/\[delorean-current\]/' /etc/yum.repos.d/delorean-current.repo
-sudo /bin/bash -c "cat <<EOF>>/etc/yum.repos.d/delorean-current.repo
-
-includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-cloud-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,tripleo-common,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements,openstack-puppet-modules
-EOF"
-
-# Enable the Delorean Deps repository
-sudo curl -o /etc/yum.repos.d/delorean-deps.repo http://trunk.rdoproject.org/centos7/delorean-deps.repo
+~/tripleo-ci/scripts/tripleo.sh --repo-setup
 
 sudo yum install -y instack-undercloud
