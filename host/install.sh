4bc308023bd83679f46802a5c8ef858e63330b54
Dougal Matthews <dougal@redhat.com>
2016-08-02 15:09:48 +0100

    Remove repo setting

--- host/install.sh
+++ host/install.sh
@@ -8,9 +8,6 @@ fi
 rm -rf ~/tripleo-ci
 git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
 
-export DELOREAN_REPO_URL="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/"
-export DELOREAN_STABLE_REPO_URL="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-$STABLE_RELEASE/"
-
 ~/tripleo-ci/scripts/tripleo.sh --repo-setup
 
 sudo yum install -y instack-undercloud
