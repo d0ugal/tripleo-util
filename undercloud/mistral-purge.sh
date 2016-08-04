ebae719fe38f9396da5962ccce11d4b76d618512
Dougal Matthews <dougal@redhat.com>
2016-08-04 13:46:50 +0100

    Remove action executions

--- undercloud/mistral-purge.sh
+++ undercloud/mistral-purge.sh
@@ -8,3 +8,10 @@ for exec_id in $exec_ids; do
     mistral execution-delete $exec_id
   fi
 done
+
+exec_ids=$(mistral action-execution-list | awk -F "| " '{print $2; }')
+for exec_id in $exec_ids; do
+  if [ ! $exec_id = "ID" ]; then
+    mistral action-execution-delete $exec_id
+  fi
+done
