64c2ecf7a5baa011c6ae525451e2e3d9daa3ab98
Dougal Matthews <dougal@redhat.com>
2016-08-15 10:15:22 +0100

    Fix

--- testenvs/local_resource_registry/run.sh
+++ testenvs/local_resource_registry/run.sh
@@ -2,5 +2,5 @@
 set -eux
 set -o pipefail
 
-~/venv/bin/openstack overcloud deployt --templates \
+~/venv/bin/openstack overcloud deploy --templates \
     -e ~/tripleo-util/local_resource_registry/wait_env.yaml
