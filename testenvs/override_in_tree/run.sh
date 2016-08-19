de2d538c23543705ccb4d9f68341fd268308da62
Dougal Matthews <dougal@redhat.com>
2016-08-19 13:55:27 +0100

    Chmod +x

--- testenvs/override_in_tree/run.sh
+++ testenvs/override_in_tree/run.sh
@@ -6,5 +6,5 @@ cp /usr/share/openstack-tripleo-heat-templates/puppet/controller.yaml \
  ~/tripleo-util/testenvs/override_in_tree/local_controller.yaml
 
 ~/venv/bin/openstack overcloud deploy --templates \
--e ~/tripleo-util/testenvs/override_in_tree/local_controller_env.yaml \
+-e ~/tripleo-util/testenvs/override_in_tree/load_controller_env.yaml \
 --debug
