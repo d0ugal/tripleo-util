1ef5860a141f398d86645515ec6ed05090e2a155
Dougal Matthews <dougal@redhat.com>
2016-08-19 10:36:28 +0100

    Split up the mistral install

--- undercloud/mistral-install.sh
+++ undercloud/mistral-install.sh
@@ -15,13 +15,4 @@ sudo mistral-db-manage populate
 # make sure the new actions got loaded
 mistral action-list | grep tripleo
 
-echo "LOADING workbooks"
-for workbook in $(mistral workbook-list | grep tripleo | cut -f 2 -d ' '); do
-    mistral workbook-delete $workbook
-done
-for workflow in $(mistral workflow-list | grep tripleo | cut -f 2 -d ' '); do
-    mistral workflow-delete $workflow
-done
-for workbook in $(ls ~/tripleo-common/workbooks/*); do
-    mistral workbook-create $workbook
-done
+~/tripleo-util/undercloud/mistral-install-workbooks.sh;
