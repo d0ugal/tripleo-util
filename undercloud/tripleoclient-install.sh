d4e12bba78f866dcc6ca3cc4d3922934b1a7bcff
Dougal Matthews <dougal@redhat.com>
2016-08-23 08:29:47 +0100

    Use a dev version of mistralclient

--- undercloud/tripleoclient-install.sh
+++ undercloud/tripleoclient-install.sh
@@ -7,3 +7,4 @@ virtualenv ~/venv;
 ~/venv/bin/pip install --editable ~/python-tripleoclient;
 ~/venv/bin/pip install tripleo-common==5.0.0.0b2 os-cloud-config==5.0.0.0b2
 ~/venv/bin/pip install git+git://github.com/openstack/python-heatclient.git@master
+~/venv/bin/pip install git+git://github.com/openstack/python-mistralclient.git@master
