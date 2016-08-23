24cee59a6d256a3b3b13e799782cb68d637e7dcf
Dougal Matthews <dougal@redhat.com>
2016-08-23 08:10:59 +0100

    Remove pin

--- undercloud/tripleoclient-install.sh
+++ undercloud/tripleoclient-install.sh
@@ -6,4 +6,3 @@ rm -rf ~/venv;
 virtualenv ~/venv;
 ~/venv/bin/pip install --editable ~/python-tripleoclient;
 ~/venv/bin/pip install tripleo-common==5.0.0.0b2 os-cloud-config==5.0.0.0b2
-~/venv/bin/pip install -UI os-client-config>=1.13.1,!=1.19.0,!=1.19.1,!=1.20.0
