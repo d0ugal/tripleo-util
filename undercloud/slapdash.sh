4a11e0f7f380b1a1ece3bcf3541a950a547f7ed3
Dougal Matthews <dougal@redhat.com>
2016-08-23 11:44:03 +0100

    List

--- undercloud/slapdash.sh
+++ undercloud/slapdash.sh
@@ -5,5 +5,7 @@ set -o pipefail
 openstack stack list;
 mistral execution-list;
 mistral action-execution-list;
+swift list;
+mistral environment-list;
 swift list overcloud | grep "user-";
 mistral environment-get overcloud;
