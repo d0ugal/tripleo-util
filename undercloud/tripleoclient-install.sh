7fc965bca0261f9937d8630ac14d08b774959f32
Dougal Matthews <dougal@redhat.com>
2016-08-30 07:34:36 +0100

    Update the client install

--- undercloud/tripleoclient-install.sh
+++ undercloud/tripleoclient-install.sh
@@ -5,6 +5,4 @@ set -o pipefail
 rm -rf ~/venv;
 virtualenv ~/venv;
 ~/venv/bin/pip install --editable ~/python-tripleoclient;
-~/venv/bin/pip install tripleo-common==5.0.0.0b2 os-cloud-config==5.0.0.0b2
-~/venv/bin/pip install git+git://github.com/openstack/python-heatclient.git@master
-~/venv/bin/pip install git+git://github.com/openstack/python-mistralclient.git@master
+~/venv/bin/pip install -r ~/python-tripleoclient/requirements.txt
