d6575863a80f987cc0c7bb19f7e05fe4bb147e61
Dougal Matthews <dougal@redhat.com>
2016-08-30 11:44:11 +0100

    Add a new test case

---
+++ test-scenarios/network_isolation/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+TRIPLEO_ROOT="~"
+~/venv/bin/openstack overcloud deploy \
+    --templates=$TRIPLEO_ROOT/tripleo-heat-templates \
+    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/network-isolation.yaml \
+    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
+    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/puppet-pacemaker.yaml \
+    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/puppet-ceph-devel.yaml \
+    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/mongodb-nojournal.yaml \
+    -e $TRIPLEO_ROOT/custom.yaml \
+    --ntp-server 94.229.74.162
