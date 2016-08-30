19553b7d09f93301e8738699ea4c3f5bc6c050ff
Dougal Matthews <dougal@redhat.com>
2016-08-30 11:47:24 +0100

    Small updates to test

--- test-scenarios/network_isolation/run.sh
+++ test-scenarios/network_isolation/run.sh
@@ -2,13 +2,14 @@
 set -eux
 set -o pipefail
 
-TRIPLEO_ROOT="~"
+TRIPLEO_ROOT=~/tripleo-heat-templates/;
+
 ~/venv/bin/openstack overcloud deploy \
-    --templates=$TRIPLEO_ROOT/tripleo-heat-templates \
-    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/network-isolation.yaml \
-    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
-    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/puppet-pacemaker.yaml \
-    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/puppet-ceph-devel.yaml \
-    -e $TRIPLEO_ROOT/tripleo-heat-templates/environments/mongodb-nojournal.yaml \
+    --templates=$TRIPLEO_ROOT \
+    -e $TRIPLEO_ROOT/environments/network-isolation.yaml \
+    -e $TRIPLEO_ROOT/environments/net-single-nic-with-vlans.yaml \
+    -e $TRIPLEO_ROOT/environments/puppet-pacemaker.yaml \
+    -e $TRIPLEO_ROOT/environments/puppet-ceph-devel.yaml \
+    -e $TRIPLEO_ROOT/environments/mongodb-nojournal.yaml \
     # -e $TRIPLEO_ROOT/custom.yaml \
-    --ntp-server 94.229.74.162
+    --ntp-server 94.229.74.162;
