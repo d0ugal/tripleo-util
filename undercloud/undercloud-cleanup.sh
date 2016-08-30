310f81f051dfa0faf4311d070e8c5024f3e6f579
Dougal Matthews <dougal@redhat.com>
2016-08-30 07:50:46 +0100

    Change the order

--- undercloud/undercloud-cleanup.sh
+++ undercloud/undercloud-cleanup.sh
@@ -3,5 +3,5 @@ set -ux
 
 swift delete overcloud;
 mistral environment-delete overcloud;
-~/tripleo-ci/scripts/tripleo.sh --overcloud-delete;
 ~/tripleo-util/undercloud/mistral-purge.sh;
+~/tripleo-ci/scripts/tripleo.sh --overcloud-delete;
