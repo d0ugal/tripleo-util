88b6f806ca7fa6ed10f80356de94c4748caa96d3
Dougal Matthews <dougal@redhat.com>
2016-09-07 08:28:06 +0100

    List the plans

--- undercloud/slapdash.sh
+++ undercloud/slapdash.sh
@@ -4,6 +4,7 @@ set -ux
 openstack stack list;
 mistral execution-list;
 mistral action-execution-list;
+openstack overcloud plan list;
 swift list;
 mistral environment-list;
 swift list overcloud | grep "user-";
