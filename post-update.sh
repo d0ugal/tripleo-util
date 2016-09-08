97ef7f0a4f944a3b0fa2c36bdc0aa582a8ebab9a
Dougal Matthews <dougal@redhat.com>
2016-09-08 10:58:11 +0100

    Typo

--- post-update.sh
+++ post-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 else
     pushd ~/.vim/bundle/Vundle.vim;
