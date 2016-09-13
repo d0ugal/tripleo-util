a99fda2772bdf0a788d2e4b9d91c8d0f97a77061
Dougal Matthews <dougal@redhat.com>
2016-09-13 15:57:26 +0100

    Change the baremetal node config

--- host/restack.sh
+++ host/restack.sh
@@ -13,8 +13,8 @@ virsh destroy instack || true;
 virsh undefine instack || true;
 
 export NODE_DIST=centos7;
-export NODE_CPU=4;
-export NODE_MEM=16384;
+export NODE_CPU=1;
+export NODE_MEM=4096;
 export UNDERCLOUD_NODE_CPU=4;
 export UNDERCLOUD_NODE_MEM=16384;
 export NODE_COUNT=6;
