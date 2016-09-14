b57a6c2cd9545332116c3e84c64313981ac9f22c
Dougal Matthews <dougal@redhat.com>
2016-09-14 16:14:49 +0100

    show user-env and user-files

--- undercloud/slapdash.sh
+++ undercloud/slapdash.sh
@@ -9,5 +9,5 @@ openstack stack list;
 mistral execution-list;
 mistral action-execution-list;
 openstack overcloud plan list;
-swift list overcloud | grep user-env;
+swift list overcloud | grep user-;
 mistral environment-get overcloud  | cut -c 1-230;
