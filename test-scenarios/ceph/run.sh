3accda38f8c35c4aabd3918871a39aa3b5a43abf
Dougal Matthews <dougal@redhat.com>
2016-09-14 15:24:48 +0100

    Home dir

--- test-scenarios/ceph/run.sh
+++ test-scenarios/ceph/run.sh
@@ -3,9 +3,9 @@ set -eux
 set -o pipefail
 
 openstack overcloud deploy \
-    --templates tripleo-heat-templates/ \
-    -e tripleo-heat-templates/environments/puppet-pacemaker.yaml \
-    -e tripleo-heat-templates/environments/storage-environment.yaml \
+    --templates ~/tripleo-heat-templates/ \
+    -e ~/tripleo-heat-templates/environments/puppet-pacemaker.yaml \
+    -e ~/tripleo-heat-templates/environments/storage-environment.yaml \
+    -e ~/tripleo-heat-templates/environments/ceph-radosgw.yaml \
     --ceph-storage-scale 1 \
-    -e tripleo-heat-templates/environments/ceph-radosgw.yaml \
     --debug;
