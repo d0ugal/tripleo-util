86e40442044567cb9bc52a7564c8044d2262b4db
Dougal Matthews <dougal@redhat.com>
2016-09-22 09:56:31 +0100

    lol

--- undercloud/mistral-install.sh
+++ undercloud/mistral-install.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 echo "INSTALLING mistral";
 cd ~/mistral;
-sudo rm -Rf /usr/lib/python2.7/site-packages/mistral*
+sudo rm -Rf /usr/lib/python2.7/site-packages/mistral/;
 sudo python setup.py install;
 cd ~/tripleo-common;
 sudo systemctl restart openstack-mistral-*;
