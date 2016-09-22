c7edb380425a8abcde217d9067278f93348fa6f6
Dougal Matthews <dougal@redhat.com>
2016-09-22 13:18:01 +0100

    Skip nones

--- undercloud/mistral-purge.sh
+++ undercloud/mistral-purge.sh
@@ -4,14 +4,14 @@ set -o pipefail
 
 exec_ids=$(mistral execution-list | grep -v sub-workflow | awk -F "| " '{print $2; }')
 for exec_id in $exec_ids; do
-  if [ ! $exec_id = "ID" ]; then
+  if [ ! $exec_id = "ID" ] && [ ! $exec_id = "<none>" ]; then
     mistral execution-delete $exec_id
   fi
 done
 
 exec_ids=$(mistral action-execution-list | awk -F "| " '{print $2; }')
 for exec_id in $exec_ids; do
-  if [ ! $exec_id = "ID" ]; then
+  if [ ! $exec_id = "ID" ] && [ ! $exec_id = "<none>" ]; then
     mistral action-execution-delete $exec_id
   fi
 done
