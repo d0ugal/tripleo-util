4a0a4c5dfde7cbab2c0ef46c95c1fa6797e3d094
Dougal Matthews <dougal@redhat.com>
2016-09-22 13:21:24 +0100

    Simplify Mistral install

--- undercloud/mistral-install.sh
+++ undercloud/mistral-install.sh
@@ -8,12 +8,3 @@ sudo rm -Rf /usr/lib/python2.7/site-packages/mistral/;
 sudo python setup.py install;
 cd ~/tripleo-common;
 sudo systemctl restart openstack-mistral-*;
-sleep 10;
-
-# this loads the actions via entrypoints
-echo  "LOADING ACTIONS";
-sudo mistral-db-manage populate
-# make sure the new actions got loaded
-mistral action-list | grep tripleo
-
-~/tripleo-util/undercloud/mistral-install-workbooks.sh;
