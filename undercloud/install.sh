c31eccf9b645ba2e339d778d3a773a780db661f8
Dougal Matthews <dougal@redhat.com>
2016-10-03 11:04:40 +0100

    Source stackrc and register nodes earlier

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -17,6 +17,9 @@ git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
 ~/tripleo-ci/scripts/tripleo.sh --undercloud
 ~/tripleo-ci/scripts/tripleo.sh --overcloud-images
 
+source ~/stackrc;
+~/tripleo-util/undercloud/openstack-register-nodes.sh
+
 wget https://bootstrap.pypa.io/get-pip.py;
 sudo python get-pip.py;
 sudo pip install virtualenv git-review;
@@ -31,8 +34,6 @@ git clone https://github.com/openstack/mistral.git;
 git clone https://github.com/d0ugal/python-tripleodash.git
 
 virtualenv ~/venv
+
 ~/tripleo-util/undercloud/tripleodash-install.sh
-
-~/tripleo-util/undercloud/openstack-register-nodes.sh
-
 ~/tripleo-util/post-update.sh
