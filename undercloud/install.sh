51d3ab14621c7c273149c810afbdd1b56203e4d5
Dougal Matthews <dougal@redhat.com>
2016-10-13 09:06:10 +0100

    Delorean

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -16,6 +16,7 @@ git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
 ~/tripleo-ci/scripts/tripleo.sh --repo-setup
 ~/tripleo-ci/scripts/tripleo.sh --undercloud
 ~/tripleo-ci/scripts/tripleo.sh --overcloud-images
+~/tripleo-ci/scripts/tripleo.sh --delorean-setup
 
 source ~/stackrc;
 ~/tripleo-util/undercloud/openstack-register-nodes.sh
@@ -28,13 +29,15 @@ git config --global user.email "dougal@redhat.com"
 git config --global user.name "Dougal Matthews"
 git config --global --add gitreview.username "dougal"
 
-git clone https://github.com/openstack/python-tripleoclient.git;
-git clone https://github.com/openstack/tripleo-common.git;
-git clone https://github.com/openstack/mistral.git;
 git clone https://github.com/d0ugal/python-tripleodash.git
 
 virtualenv ~/venv
 
 ~/tripleo-util/undercloud/tripleodash-install.sh
-~/tripleo-util/undercloud/tripleoclient-install.sh
 ~/tripleo-util/post-update.sh
+
+~/tripleo-ci/scripts/tripleo.sh --delorean-build openstack/instack-undercloud
+~/tripleo-ci/scripts/tripleo.sh --delorean-build openstack/python-tripleoclient
+~/tripleo-ci/scripts/tripleo.sh --delorean-build openstack/tripleo-common
+~/tripleo-ci/scripts/tripleo.sh --delorean-build openstack/mistral
+~/tripleo-ci/scripts/tripleo.sh --delorean-build openstack/python-mistralclient
