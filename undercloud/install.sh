3e268bab7b69113804f113ee83ccfb5acd5a13ee
Dougal Matthews <dougal@redhat.com>
2017-01-23 13:52:39 +0000

    Add a new setup script, move stuff out of install.
    It is now easier to make changes or jump in at an earlier stage

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -3,8 +3,8 @@ set -eux
 set -o pipefail
 
 if [[ $USER != "stack" ]]; then
-  echo "Must be run as stack user.";
-	exit 1;
+    echo "Must be run as stack user.";
+    exit 1;
 fi
 
 sudo yum upgrade -y;
@@ -18,24 +18,3 @@ git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
 ~/tripleo-ci/scripts/tripleo.sh --undercloud
 ~/tripleo-ci/scripts/tripleo.sh --overcloud-images
 ~/tripleo-ci/scripts/tripleo.sh --delorean-setup
-
-source ~/stackrc;
-~/tripleo-util/undercloud/openstack-register-nodes.sh
-
-wget https://bootstrap.pypa.io/get-pip.py;
-sudo python get-pip.py;
-sudo pip install -IU virtualenv setuptools git-review;
-
-git config --global user.email "dougal@redhat.com"
-git config --global user.name "Dougal Matthews"
-git config --global --add gitreview.username "dougal"
-
-mkdir -p ~/code;
-pushd ~/code;
-git clone https://github.com/d0ugal/python-tripleodash.git
-popd;
-
-virtualenv ~/venv
-
-~/tripleo-util/undercloud/tripleodash-install.sh
-~/tripleo-util/post-update.sh
---
+++ undercloud/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+if [[ $USER != "stack" ]]; then
+    echo "Must be run as stack user.";
+    exit 1;
+fi
+
+source ~/stackrc;
+~/tripleo-util/undercloud/openstack-register-nodes.sh
+
+wget https://bootstrap.pypa.io/get-pip.py;
+sudo python get-pip.py;
+sudo pip install -IU virtualenv setuptools git-review;
+
+git config --global user.email "dougal@redhat.com"
+git config --global user.name "Dougal Matthews"
+git config --global --add gitreview.username "dougal"
+
+mkdir -p ~/code;
+pushd ~/code;
+git clone https://github.com/d0ugal/python-tripleodash.git
+popd;
+
+virtualenv ~/venv
+
+~/tripleo-util/undercloud/tripleodash-install.sh
+~/tripleo-util/post-update.sh
