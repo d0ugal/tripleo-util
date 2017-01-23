13192223da28cc8642fa1ae68572a21ea4063238
Dougal Matthews <dougal@redhat.com>
2017-01-23 14:43:14 +0000

    Add some steps back to the install script

--- host/install.sh
+++ host/install.sh
@@ -3,8 +3,8 @@ set -eux
 set -o pipefail
 
 if [[ $USER != "stack" ]]; then
-  echo "Must be run as stack user.";
-	exit 1;
+    echo "Must be run as stack user.";
+    exit 1;
 fi
 
 sudo yum install -y yum-utils;
@@ -15,3 +15,11 @@ git clone https://github.com/openstack-infra/tripleo-ci.git ~/tripleo-ci
 ~/tripleo-ci/scripts/tripleo.sh --repo-setup
 
 sudo yum install -y epel-release instack-undercloud
+
+wget https://bootstrap.pypa.io/get-pip.py;
+sudo python get-pip.py;
+sudo pip install -IU virtualenv setuptools git-review;
+
+git config --global user.email "dougal@redhat.com"
+git config --global user.name "Dougal Matthews"
+git config --global --add gitreview.username "dougal"
