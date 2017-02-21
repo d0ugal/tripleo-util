e1fbe3966d99c2f73fa6690b8fc5c96cc22d31b4
Dougal Matthews <dougal@redhat.com>
2017-02-21 11:05:06 +0000

    Two stage restacking :(

--- host/restack.sh
+++ host/restack.sh
@@ -8,7 +8,3 @@ if [[ $USER != "stack" ]]; then
 fi
 
 ~/quickstart.sh --teardown all 127.0.0.2 || true;
-# http://i1.kym-cdn.com/photos/images/facebook/000/511/991/3a5.jpg
-rm -rf ~/*;
-git clone https://github.com/d0ugal/tripleo-util.git;
-~/tripleo-util/host/install.sh;
---
+++ host/restack2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+if [[ $USER != "stack" ]]; then
+    echo "Must be run as stack user.";
+    exit 1;
+fi
+
+# http://i1.kym-cdn.com/photos/images/facebook/000/511/991/3a5.jpg
+rm -rf ~/*;
+git clone https://github.com/d0ugal/tripleo-util.git;
+~/tripleo-util/host/install.sh;
