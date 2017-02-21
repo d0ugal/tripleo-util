135f67d4c87a624d8a3fa9c3384e5ed1eb6c15d3
Dougal Matthews <dougal@redhat.com>
2017-02-21 08:13:20 +0000

    wget

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -7,6 +7,8 @@ if [[ $USER != "stack" ]]; then
     exit 1;
 fi
 
+sudo yum install -y wget;
+
 git clone https://github.com/openstack-infra/tripleo-ci.git;
 
 wget https://bootstrap.pypa.io/get-pip.py;
