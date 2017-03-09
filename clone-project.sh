c9ad0c9f90192b865b3bdf38ed140c93912dcb5a
Dougal Matthews <dougal@redhat.com>
2017-03-09 10:49:43 +0000

    I bash good

--- clone-project.sh
+++ clone-project.sh
@@ -2,7 +2,7 @@
 set -eux
 set -o pipefail
 
-ROOT=${TRIPLEO_ROOT:-$HOME/code}
+ROOT=${ROOT:-$HOME/code}
 mkdir -p $ROOT
 
 PROJ=$1
@@ -10,20 +10,20 @@ REVIEW=${2:-""}
 
 
 # Clone the repo if it doesn't yet exist
-if [ ! -d $TRIPLEO_ROOT/$PROJ ]; then
+if [ ! -d $ROOT/$PROJ ]; then
     echo "Cloning the repo..."
-    git clone https://git.openstack.org/openstack/$PROJ.git $TRIPLEO_ROOT/$PROJ
+    git clone https://git.openstack.org/openstack/$PROJ.git $ROOT/$PROJ
 fi
 
 if [ ! -z $REVIEW ]; then
     echo "Getting review...";
-    pushd $TRIPLEO_ROOT/$PROJ;
+    pushd $ROOT/$PROJ;
     git review -d $REVIEW;
     git show -s;
     popd;
 else
     echo "Installing checked out version..."
-    pushd $TRIPLEO_ROOT/$PROJ;
+    pushd $ROOT/$PROJ;
     git show -s;
     popd;
 fi
