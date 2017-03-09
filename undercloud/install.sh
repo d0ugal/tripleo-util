59cfa71bb127b857a2823b2f88b483a1369b9c32
Dougal Matthews <dougal@redhat.com>
2017-03-09 10:56:30 +0000

    Install more

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install -y python-virtualenv python-pip
+sudo yum install -y python-virtualenv python-pip wget gcc vim
 sudo pip install git-review
 
 if [ ! -f ~/venv ]; then
