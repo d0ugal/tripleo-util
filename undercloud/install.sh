12c5ae4da13d2c2b15dc629a53c9c24416faff13
Dougal Matthews <dougal@redhat.com>
2017-03-09 10:36:12 +0000

    Remove cruft

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -4,31 +4,16 @@ set -o pipefail
 
 sudo yum install -y wget python-dev gcc vim;
 
-if [ -d ~/tripleo-ci ]; then
-    rm -rf ~/tripleo-ci;
-fi
-
-git clone https://github.com/openstack-infra/tripleo-ci.git;
-~/tripleo-ci/scripts/tripleo.sh --delorean-setup;
-
 if [ ! -f ~/get-pip.py ]; then
     wget https://bootstrap.pypa.io/get-pip.py;
     sudo python get-pip.py;
-    sudo pip install -IU virtualenv setuptools git-review;
+    sudo pip install -IU virtualenv git-review;
 fi
 
 git config --global user.email "dougal@redhat.com"
 git config --global user.name "Dougal Matthews"
 git config --global --add gitreview.username "dougal"
 
-mkdir -p ~/code;
-pushd ~/code;
-if [ ! -d python-tripleodash ]; then
-    git clone https://github.com/d0ugal/python-tripleodash.git;
-fi
-popd;
-
 virtualenv ~/venv
 
-~/tripleo-util/undercloud/tripleodash-install.sh
 ~/tripleo-util/dotfiles/install.sh
