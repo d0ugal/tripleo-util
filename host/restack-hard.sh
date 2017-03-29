b27ea22a4ae7a7cdd9837d206b2bbd8590ee3396
Dougal Matthews <dougal@redhat.com>
2017-03-29 09:49:25 +0100

    Smarter cleanup

--- host/restack-hard.sh
+++ host/restack-hard.sh
@@ -4,6 +4,7 @@ set -o pipefail
 
 ~/tripleo-util/host/teardown.sh;
 # http://i1.kym-cdn.com/photos/images/facebook/000/511/991/3a5.jpg
-rm -rf ~/*;
+find . ! -name 'tripleo-util' -type d -exec rm -rf {} +;
+~/tripleo-util/update.sh;
 git clone https://github.com/d0ugal/tripleo-util.git;
 ~/tripleo-util/host/install.sh;
