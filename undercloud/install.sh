470ee13852d4ef490a737e2e9065e6e25fa6f306
Dougal Matthews <dougal@redhat.com>
2017-05-02 12:46:49 +0100

    Explicitly install six

--- undercloud/install.sh
+++ undercloud/install.sh
@@ -8,7 +8,7 @@ if [ ! -f ~/venv ]; then
 fi
 
 source ~/venv/bin/activate
-pip install -U pip setuptools
+pip install -U six pip setuptools
 
 git config --global user.email "dougal@redhat.com"
 git config --global user.name "Dougal Matthews"
