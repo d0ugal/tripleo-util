07f07f527adf503378e4f2c11dec741447a61929
Dougal Matthews <dougal@redhat.com>
2018-01-25 10:57:55 +0000

    Revert "new?"
    This reverts commit 801584402269cc137bb23d1fba371981b3b48d34.

--- clean.sh
+++ clean.sh
@@ -9,6 +9,22 @@ fi
 
 sudo yum -y update;
 
+rm -rf ~/tripleo-quickstart;
+rm -rf ~/.quickstart ;
+rm -rf ~/.ansible
+rm -rf ~/.ara
+rm -rf ~/.novaclient
+rm -rf ~/.pki
+
+# We want the root user to have the same keys as us.
+if [ ! -f ~/.ssh/id_rsa ]; then
+    ssh-keygen -f ~/.ssh/id_rsa -t rsa -N '';
+fi
+sudo rm -rf /root/.ssh;
+sudo cp -r ~/.ssh/ /root/.ssh && sudo chown -R root /root/.ssh;
+
+dt="$(date "+%Y-%m-%d_%H-%M_%s")";
+
 source ~/openrc.sh;
 
 # Manually remove ports and old stacks.
@@ -19,3 +35,7 @@ openstack port list --long;
 openstack stack list;
 openstack stack delete -y --wait $(openstack stack list -c ID -f value) || true;
 openstack stack list;
+
+git clone https://github.com/openstack/tripleo-quickstart.git ~/tripleo-quickstart;
+
+unbuffer bash ~/tripleo-quickstart/quickstart.sh --install-deps 2>&1 | tee -a logs/$dt.install.log;
