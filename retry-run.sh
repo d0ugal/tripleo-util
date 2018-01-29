07c14efa07adb6a4a18ef6f4704a94e54349f948
Dougal Matthews <dougal@redhat.com>
2018-01-29 16:16:41 +0000

    Retry deploying until we have a clean run

---
+++ retry-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+if [ ! -f /.dockerenv ]; then
+    echo "This must be run within a docker container";
+    exit 0;
+fi
+
+while true
+do
+  bash run.sh $1 && break
+done
