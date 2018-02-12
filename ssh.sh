50ff61d6a4565f239f182b1df4e80c8df09af51d
Dougal Matthews <dougal@redhat.com>
2018-02-12 15:21:23 +0000

    ssh with keys - now works outside the container

--- ssh.sh
+++ ssh.sh
@@ -2,4 +2,6 @@
 set -eux
 set -o pipefail
 
-export $(awk '/subnode-0/ {print $2}' reproduce/multinode_hosts); ssh zuul@$ansible_host
+export $(awk '/subnode-0/ {print $2}' reproduce/multinode_hosts);
+
+ssh zuul@$ansible_host -i .ssh/id_rsa
