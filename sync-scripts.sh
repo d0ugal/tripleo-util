2c07c6ed423e1bbdab3f7938cd1fa1c37dbff040
Dougal Matthews <dougal@redhat.com>
2018-03-19 08:55:11 +0000

    Update scripts

--- sync-scripts.sh
+++ sync-scripts.sh
@@ -10,5 +10,5 @@ fi
 export $(awk '/subnode-0/ {print $2}' reproduce/multinode_hosts);
 
 ssh-keygen -R $ansible_host
-rsync -arvce "ssh -o StrictHostKeyChecking=no" -r zuul@$ansible_host:~/scripts ~/
 rsync -arvce "ssh -o StrictHostKeyChecking=no" -r ~/scripts zuul@$ansible_host:~/
+rsync -arvce "ssh -o StrictHostKeyChecking=no" -r zuul@$ansible_host:~/scripts ~/
