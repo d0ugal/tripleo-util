3b373f91135e4fb246d4a027d197a8bf57f2acc0
Dougal Matthews <dougal@redhat.com>
2018-04-04 10:56:39 +0100

    Start storing testing workbooks

--- scripts/mistral-delete-executions.sh
+++ scripts/mistral-delete-executions.sh
@@ -9,6 +9,7 @@ for exec_id in $exec_ids; do
   fi
 done
 
+# Action execution deletion is disabled by default, so this may fail.
 exec_ids=$(mistral action-execution-list | awk -F "| " '{print $2; }')
 for exec_id in $exec_ids; do
   if [ ! $exec_id = "ID" ] && [ ! $exec_id = "<none>" ]; then
--- scripts/mistral-install-workbooks.sh
+++ scripts/mistral-install-workbooks.sh
@@ -2,13 +2,6 @@
 set -eux
 set -o pipefail
 
-echo "LOADING workbooks"
-for workbook in $(mistral workbook-list | grep tripleo | cut -f 2 -d ' '); do
-    mistral workbook-delete $workbook
-done
-for workflow in $(mistral workflow-list | grep tripleo | cut -f 2 -d ' '); do
-    mistral workflow-delete $workflow
-done
-for workbook in $(ls ~/code/tripleo-common/workbooks/*); do
-    mistral workbook-create $workbook
+for workbook in $(ls ~/scripts/workbooks/*); do
+    mistral workbook-create $workbook || mistral workbook-update $workbook;
 done
