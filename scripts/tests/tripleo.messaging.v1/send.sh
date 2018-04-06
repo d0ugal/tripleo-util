3f7b5beaf0f8186ee98fbf92058455926272d0d8
Dougal Matthews <dougal@redhat.com>
2018-04-06 13:23:06 +0100

    Smaller

--- scripts/tests/tripleo.messaging.v1/send.sh
+++ scripts/tests/tripleo.messaging.v1/send.sh
@@ -8,35 +8,38 @@ create="openstack workflow execution create -c ID -f value";
 show="openstack workflow execution show";
 state="$show -c State -f value"
 wf="tripleo.messaging.v1.send"
-
-wait_for_execution(){
-    ex_state=$($state $1);
-    while [ "$ex_state" != "SUCCESS"  ] && [ "$ex_state" != "ERROR"  ]; do
-      ex_state=$($state $1);
-    done
-    ~/scripts/wf-report $1;
-    $show $1;
-    sleep 10;
-}
-
 swift delete overcloud-messages || true;
 
-json='{"type": "test", "queue_name": "tripleo", "plan_name": "overcloud", "execution": {"id": "UUID"}, "plan_status": "statusseses"}';
-ex_id=$($create $wf "$json" -d "Plan name and status. No container");
-wait_for_execution $ex_id;
+run_and_wait(){
+    ex_id=$($create $wf "$2" -d "$1");
+    ex_state=$($state $ex_id);
+    while [ "$ex_state" != "SUCCESS"  ] && [ "$ex_state" != "ERROR"  ]; do
+      ex_state=$($state $ex_id);
+    done
+    ~/scripts/wf-report $ex_id;
+    $show $ex_id;
+    if [ "$ex_state" != "SUCCESS" ]; then
+      echo "Execution failed";
+      exit 42;
+    fi
+}
 
-json='{"type": "test", "queue_name": "tripleo", "plan_name": "overcloud", "execution": {"id": "UUID"}, "plan_status": "statusseses"}';
-ex_id=$($create $wf "$json" -d "Plan name and status. container exists");
-wait_for_execution $ex_id;
+run_and_wait \
+  "Plan name and status. No container" \
+  '{"type": "test", "queue_name": "tripleo", "plan_name": "overcloud", "execution": {"id": "UUID"}, "plan_status": "statusseses"}';
 
-json='{"type": "test", "queue_name": "tripleo", "plan_name": "overcloud", "execution": {"id": "UUID"}}';
-ex_id=$($create $wf "$json" -d "Plan name, no status.");
-wait_for_execution $ex_id;
+run_and_wait \
+  "Plan name and status. container exists" \
+  '{"type": "test", "queue_name": "tripleo", "plan_name": "overcloud", "execution": {"id": "UUID"}, "plan_status": "statusseses"}';
 
-json='{"type": "test", "queue_name": "tripleo", "execution": {"id": "UUID"}}';
-ex_id=$($create $wf "$json" -d "No plan or status");
-wait_for_execution $ex_id;
+run_and_wait \
+  "Plan name, no status." \
+  '{"type": "test", "queue_name": "tripleo", "plan_name": "overcloud", "execution": {"id": "UUID"}}';
 
-json='{"type": "test", "queue_name": "tripleo", "execution": {"id": "UUID"}, "plan_status": "status"}';
-ex_id=$($create $wf "$json" -d "Plan status but no plan name");
-wait_for_execution $ex_id;
+run_and_wait \
+  "No plan or status" \
+  '{"type": "test", "queue_name": "tripleo", "execution": {"id": "UUID"}}';
+
+run_and_wait \
+  "Plan status but no plan name" \
+  '{"type": "test", "queue_name": "tripleo", "execution": {"id": "UUID"}, "plan_status": "status"}';
