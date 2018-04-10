956ee08e3ef48fde0cfd846fc1a7dc444333e5f6
Dougal Matthews <dougal@redhat.com>
2018-04-10 16:02:58 +0100

    Add a test script for the swift container utility

---
+++ scripts/tests/tripleo.swift.v1/container_exists.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+~/scripts/mistral-delete-executions.sh;
+
+create="openstack workflow execution create -c ID -f value";
+show="openstack workflow execution show";
+state="$show -c State -f value"
+wf="tripleo.messaging.v1.container_exists"
+
+_run_and_wait(){
+    expected=${3:-SUCCESS};
+
+    ex_id=$($create $wf "$2" -d "$1");
+    ex_state=$($state $ex_id);
+    while [ "$ex_state" != "SUCCESS"  ] && [ "$ex_state" != "ERROR"  ]; do
+      ex_state=$($state $ex_id);
+    done
+    ~/scripts/wf-report $ex_id;
+    $show $ex_id;
+    if [ "$ex_state" != "$expected" ]; then
+      echo "Execution failed";
+      exit 42;
+    fi
+}
+
+swift delete test_container || true;
+
+run_and_wait "No container, No create" '{"name":"test_container"}';
+run_and_wait "No container, Yes create" '{"name":"test_container", "create_container":true}';
+run_and_wait "Yes container, No create" '{"name":"test_container"}';
+run_and_wait "Yes container, Yes create" '{"name":"test_container", "create_container":true}';
