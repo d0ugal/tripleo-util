68291073dfd03bd928d6e1ce003a8e0ba21959b5
Dougal Matthews <dougal@redhat.com>
2018-04-11 10:22:13 +0100

    updates

--- ssh.sh
+++ ssh.sh
@@ -8,5 +8,7 @@ else
     export $(awk '/subnode-0/ {print $2}' build/ovb_hosts);
 fi;
 
+./sync-scripts.sh
+
 ssh-keygen -R $ansible_host
 ssh -o StrictHostKeyChecking=no zuul@$ansible_host -i .ssh/id_rsa
