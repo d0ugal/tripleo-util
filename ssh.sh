dd5725073428e9aad7617743059d7fb18f9642a7
Dougal Matthews <dougal@redhat.com>
2018-06-11 11:28:48 +0100

    Must run in docker

--- ssh.sh
+++ ssh.sh
@@ -2,6 +2,11 @@
 set -eux
 set -o pipefail
 
+if [ ! -f /.dockerenv ]; then
+    echo "This must be run within a docker container";
+    exit 0;
+fi
+
 WORKSPACE=~/build;
 
 if [ -f $WORKSPACE/multinode_hosts ]; then
