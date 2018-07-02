5f684ac563fb4a032f0cff854779f68c8bc2b4f3
Dougal Matthews <dougal@redhat.com>
2018-07-02 16:37:38 +0100

    Working

--- scripts/install-review.sh
+++ scripts/install-review.sh
@@ -4,13 +4,14 @@ set -o pipefail
 
 id=$1
 url="https://review.openstack.org/changes/?q=$id&o=DOWNLOAD_COMMANDS&o=CURRENT_REVISION"
+# Because gerrit hates users...
 json=$(curl -s $url | sed 's/^.\{4\}//g');
 git_url=$(echo $json | jq ".[0].revisions[.[0].current_revision].fetch.\"anonymous http\".url" -r);
 git_command=$(echo $json | jq ".[0].revisions[.[0].current_revision].fetch.\"anonymous http\".commands.Checkout" -r);
+IFS='&' read -a git_command_parts <<< $git_command;
 
 cd $(mktemp -d);
 git clone $git_url repo;
 cd repo;
-echo $git_command;
-$git_command;
+${git_command_parts[0]} && ${git_command_parts[2]};
 sudo pip install --no-deps .;
