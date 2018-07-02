6a3311718347f2c658be381230b373802a0d21b6
Dougal Matthews <dougal@redhat.com>
2018-07-02 16:18:12 +0100

    oops

--- scripts/install-review.sh
+++ scripts/install-review.sh
@@ -6,9 +6,11 @@ id=$1
 url="https://review.openstack.org/changes/?q=$id&o=DOWNLOAD_COMMANDS&o=CURRENT_REVISION"
 json=$(curl -s $url | sed 's/^.\{4\}//g');
 git_url=$(echo $json | jq ".[0].revisions[.[0].current_revision].fetch.\"anonymous http\".url" -r);
-git_command=$(echo $json | jq ".[0].revisions[.[0].current_revision].fetch.\"anonymous http\".git.Checkout" -r);
+git_command=$(echo $json | jq ".[0].revisions[.[0].current_revision].fetch.\"anonymous http\".commands.Checkout" -r);
 
 cd $(mktemp -d);
 git clone $git_url repo;
 cd repo;
+echo $git_command;
+$git_command;
 sudo pip install --no-deps .;
